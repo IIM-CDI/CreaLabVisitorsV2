e506221b07489403c799551a745856a43094f0d4
Thomas Candille <thomascandille0508@gmail.com>
2026-06-02 15:16:16 +0200

    chore: initialize project with package.json and basic configurations

---
+++ eslint.config.ts
@@ -0,0 +1,14 @@
+// eslint.config.js
+import { defineConfig } from "eslint/config";
+
+export default defineConfig([
+	{
+		rules: {
+			semi: "error",
+			"prefer-const": "error",
+		},
+    extends: [
+      "prettier",
+    ],
+	},
+]);
