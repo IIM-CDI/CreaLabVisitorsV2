cf4c539e21e06577607cb6ff540711f6c493326a
Thomas Candille <thomascandille0508@gmail.com>
2026-06-17 12:04:42 +0200

    feat: add FullCalendar integration with user-specific views and styles

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,15 +1,17 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
 
 import LoginLayout from './Layout/LoginLayout/LoginLayout';
 import CalendarLayout from './Layout/CalendarLayout/CalendarLayout';
 
 function App() {
+    localStorage.setItem('user', 'John Doe'); // Set a default user for demonstration purposes
+    const [user, setUser] = useState(localStorage.getItem('user') || null);
+
     return (
         <div className="App">
             <div className="background" />
-            <LoginLayout />
-            <CalendarLayout />
+            {user ? <CalendarLayout user={user} /> : <LoginLayout />}
         </div>
     );
 }
--- frontend/src/Layout/CalendarLayout/CalendarLayout.tsx
+++ frontend/src/Layout/CalendarLayout/CalendarLayout.tsx
@@ -1,10 +1,60 @@
 import React from 'react';
+import Fullcalendar from '@fullcalendar/react';
+import dayGridPlugin from '@fullcalendar/daygrid';
+import timeGridPlugin from '@fullcalendar/timegrid';
+import interactionPlugin from '@fullcalendar/interaction';
 import './CalendarLayout.css';
 
-const CalendarLayout = () => {
+interface CalendarLayoutProps {
+    user: string;
+}
+
+const CalendarLayout = ({ user }: CalendarLayoutProps) => {
+
+    const calendarConfig = {
+        headerToolbar: {
+            left: "prev,next today",
+            center: "title",
+            right: "timeGridDay,timeGridWeek,dayGridMonth"
+        },
+        buttonText: {
+            today: 'Aujourd\'hui',
+            month: 'Mois',
+            week: 'Semaine',
+            day: 'Jour'
+        },
+        initialView: "timeGridWeek",
+        firstDay: 1,
+        slotLabelFormat: {
+            hour: '2-digit' as const,
+            minute: '2-digit' as const,
+            hour12: false as const
+        },
+        eventTimeFormat: {
+            hour: '2-digit' as const,
+            minute: '2-digit' as const,
+            hour12: false as const
+        },
+        slotMinTime: "08:00:00",
+        slotMaxTime: "20:00:00",
+        allDaySlot: false,
+        editable: true,
+        selectable: true,
+        weekends: false,
+    };
+
     return (
         <div className="calendar-layout">
-            <h1>C</h1>
+            <div className="navbar">
+                <h1>Bienvenue au CreaLab {user}</h1>
+            </div>
+            
+            <div className="calendar-container">
+                <Fullcalendar
+                    plugins={[dayGridPlugin, timeGridPlugin, interactionPlugin]}
+                    {...calendarConfig}
+                />
+            </div>
         </div>
     );
 };
