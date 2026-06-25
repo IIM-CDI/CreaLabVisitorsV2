00ce6dc5489592418c2e4f92fbda11122daeaae2
Thomas Candille <thomascandille0508@gmail.com>
2026-06-25 14:43:30 +0200

    fix:Cors policy

--- frontend/src/hooks/useAPI.ts
+++ frontend/src/hooks/useAPI.ts
@@ -9,6 +9,8 @@ export const useApi = () => {
         const headers: Record<string, string> = {
             'Content-Type': 'application/json',
             'Access-Control-Allow-Origin': '*',
+            'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE',
+            'Access-Control-Allow-Headers': 'Content-Type, Authorization'
         };
         return headers;
     }, []);
